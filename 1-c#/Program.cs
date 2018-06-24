422acf338dab857a195ab304d2297d1feab637b1
JAreina <jareinafdez@gmail.com>
2018-06-24 14:56:43 +0200

    hola mundo

---
+++ 1-c#/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
