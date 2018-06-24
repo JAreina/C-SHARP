c36f442c6ea15ae9c9fd51be4b765732bb91f8a8
JAreina <jareinafdez@gmail.com>
2018-06-24 19:44:23 +0200

    cadenas

---
+++ 3-cadenas/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _3_cadenas {
+    class Program {
+        static void Main (string[] args) {
+            string saludo = "Hola";
+            string segundoSaludo;
+            string nombre, despedida;
+
+            segundoSaludo = "Que tal?";
+            Console.WriteLine ("Dime tu nombre... ");
+            nombre = Console.ReadLine ();
+
+        
+            Console.WriteLine ("{0} {1}", saludo, nombre);
+            Console.WriteLine (segundoSaludo);
+
+            if (nombre == "Rajoy")
+                Console.WriteLine ("Dices que eres Rajoy?");
+            else
+                Console.WriteLine ("Así que no eres Rajoy?");
+
+            despedida = "Adios " + nombre + "!";
+            Console.WriteLine (despedida);
+
+
+             /* length string */
+
+            Console.WriteLine (nombre.Length + " : longitud de la cadena introducida");
+
+            /* substring */
+            
+            Console.WriteLine (nombre.Substring (0, 1));
+
+            /* indexof */
+
+            string quixote = "En un lugar de la Mancha de cuyo nombre ...";
+            if (quixote.IndexOf ("Mancha") != -1) {
+                Console.WriteLine ("existe la palabra Mancha");
+            }
+            if(quixote.Contains("cuyo")) Console.WriteLine("APARECE LA PALABRA \"cuyo\"");
+        }
+    }
+}
---
+++ 4-cadenas/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _4_cadenas {
+    class Program {
+        static void Main (string[] args) {
+            string ejemplo = "better than you, better than you";
+            Console.WriteLine ("El texto es: {0}",
+                ejemplo);
+            Console.WriteLine ("La primera letra es: {0}",
+                ejemplo[0]);
+            Console.WriteLine ("Las tres primeras letras son: {0}",
+                ejemplo.Substring (0, 3));
+            Console.WriteLine ("La longitud del texto es: {0}",
+                ejemplo.Length);
+            Console.WriteLine ("La posicion de \"a\" es: {0}",
+                ejemplo.IndexOf ("a"));
+            Console.WriteLine ("La ultima u esta en la posicion: {0}", ejemplo.LastIndexOf ("u"));
+            Console.WriteLine ("En mayúsculas: {0}",
+                ejemplo.ToUpper ());
+            Console.WriteLine ("En minúsculas: {0}",
+                ejemplo.ToLower ());
+            Console.WriteLine ("Si insertamos \", YOU\": {0}",
+                ejemplo.Insert (ejemplo.Length, " YOU"));
+            Console.WriteLine ("Si borramos las 6 primeras letras: {0}",
+                ejemplo.Remove (0, 6));
+            Console.WriteLine ("Si cambiamos YOU por ALL: {0}",
+                ejemplo.Replace ("you", "ALL"));
+        }
+    }
+}
