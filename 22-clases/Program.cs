18d487b0632b086bd96dc13d7bdd4ff1ccafad0e
JAreina <jareinafdez@gmail.com>
2018-06-25 18:51:57 +0200

    clases

---
+++ 22-clases/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _22_clases {
+    
+    class Puerta {
+        int ancho; // Ancho en centimetros
+        int alto; // Alto en centimetros
+        int color; // Color en formato RGB
+        bool abierta; // Abierta o cerrada
+        public void Abrir () {
+            abierta = true;
+        }
+        public void Cerrar () {
+            abierta = false;
+        }
+        public void MostrarEstado () {
+            Console.WriteLine ("Ancho: {0}", ancho);
+            Console.WriteLine ("Alto: {0}", alto);
+            Console.WriteLine ("Color: {0}", color);
+            Console.WriteLine ("Abierta: {0}", abierta);
+        }
+    } // Final de la clase Puerta
+
+    public class Principal {
+        public static void Main () {
+            Puerta p = new Puerta ();
+            Console.WriteLine ("Valores iniciales...");
+            p.MostrarEstado ();
+            Console.WriteLine ("\nVamos a abrir...");
+            p.Abrir ();
+            p.MostrarEstado ();
+        }
+    }
+}
