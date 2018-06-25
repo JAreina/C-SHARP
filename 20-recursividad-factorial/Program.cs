7c35c2ff8e81944ca45e2ff0d3e1ebff7506dd91
JAreina <jareinafdez@gmail.com>
2018-06-25 18:36:59 +0200

    recursividad factorial

--- 20-recursividad-factorial/Program.cs
+++ 20-recursividad-factorial/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 
-namespace _20_recursividad_factorial
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace _20_recursividad_factorial {
+    class Program {
+        public static long fact (int n) {
+            if (n == 1) // Aseguramos que termine
+                return 1;
+            return n * fact (n - 1); // Si no es 1, sigue la recursión
+        }
+        
+        public static void Main () {
+            int num;
+            Console.WriteLine ("Introduzca un número entero: ");
+            num = Convert.ToInt32 (System.Console.ReadLine ());
+            Console.WriteLine ("Su factorial es: {0}", fact (num));
         }
     }
 }
