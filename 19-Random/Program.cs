71347d79671cae563f455390ec21aadd41f84bc9
JAreina <jareinafdez@gmail.com>
2018-06-25 18:35:59 +0200

    random

--- 19-Random/Program.cs
+++ 19-Random/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 
-namespace _19_Random
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace _19_Random {
+    class Program {
+
+        public static void Main () {
+            Random r = new Random ();
+            int aleatorio = r.Next (1, 11);
+            Console.WriteLine ("Un número entre 1 y 10: {0}", aleatorio);
+            int aleatorio2 = r.Next (1, 191);
+            Console.WriteLine ("Otro: {0}", aleatorio2);
         }
     }
 }
---
+++ 20-recursividad-factorial/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _20_recursividad_factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
