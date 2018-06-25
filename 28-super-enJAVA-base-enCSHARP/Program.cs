0d0e67ad70df8401c6343f147283bc7ac8dea1b8
JAreina <jareinafdez@gmail.com>
2018-06-25 19:54:36 +0200

    base para llamar a metodo padre en herencia

---
+++ 28-super-enJAVA-base-enCSHARP/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _28_super_enJAVA_base_enCSHARP {
+    public class Animal { }
+    // ------------------
+    public class Gato : Animal {
+        public void Hablar () {
+            Console.WriteLine ("Miauuu");
+        }
+    }
+    // ------------------
+   
+    public class GatoSiames : Gato {
+        public new void Hablar () {
+            base.Hablar ();
+            Console.WriteLine ("Pfff");
+        }
+    }
+    // ------------------
+    public class Ejemplo67 {
+        public static void Main () {
+            Gato miGato = new Gato ();
+            GatoSiames miGato2 = new GatoSiames ();
+            miGato.Hablar ();
+            Console.WriteLine (); // Linea en blanco
+            miGato2.Hablar ();
+        }
+    }
+}
