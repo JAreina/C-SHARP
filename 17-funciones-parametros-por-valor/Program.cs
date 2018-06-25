81b18a8af089f7d38e411e6c06247369f6112ab4
JAreina <jareinafdez@gmail.com>
2018-06-25 18:32:13 +0200

    por valor, por referencia

---
+++ 17-funciones-parametros-por-valor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _17_funciones_parametros_por_valor {
+    class Program {
+        public static void duplica (int x) {
+            Console.WriteLine (" El valor recibido por valor  vale {0}", x);
+            x = x * 2;
+            Console.WriteLine (" y ahora vale {0}", x);
+        }
+        public static void Main () {
+            int n = 5;
+            Console.WriteLine ("n vale {0}", n);
+            duplica (n);
+            Console.WriteLine ("Ahora n sigue valiendo  {0} ", n);
+        }
+    }
+}
---
+++ 18-funciones-parametros-por-referencia/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _18_funciones_parametros_por_referencia {
+    class Program {
+
+        public static void duplica (ref int x) {
+            Console.WriteLine (" El valor recibido por referencia vale {0}", x);
+            x = x * 2;
+            Console.WriteLine (" y ahora vale funcion duplica {0}", x);
+        }
+        public static void Main () {
+            int n = 5;
+            Console.WriteLine ("main: n vale {0}", n);
+            duplica (ref n);
+            Console.WriteLine ("main: Ahora n HA CAMBIADO {0}", n);
+        }
+    }
+}
