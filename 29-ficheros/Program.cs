5ce67ffed0762c22a12b0f95cce906114596097d
JAreina <jareinafdez@gmail.com>
2018-06-25 20:33:21 +0200

    ficheros, excepciones,try catch

--- 29-ficheros/Program.cs
+++ 29-ficheros/Program.cs
@@ -7,12 +7,73 @@ public class Ejemplo70
 {
 public static void Main()
 {
+
+    // ESCRIBIR 
 StreamWriter fichero;
 fichero = File.CreateText("prueba.txt");
 fichero.WriteLine("Esto es una línea");
 fichero.Write("Esto es otra");
 fichero.WriteLine(" y esto es continuación de la anterior");
 fichero.Close();
+
+
+// LEER 
+
+StreamReader fichero2;
+string linea;
+fichero2 = File.OpenText("prueba.txt");
+linea = fichero2.ReadLine();
+Console.WriteLine( linea );
+Console.WriteLine( fichero2.ReadLine() );
+fichero2.Close();
+
+
+// LEER ENTERO EL FICHERO
+
+StreamReader fichero3;
+string linea2;
+fichero3 = File.OpenText("prueba.txt");
+do
+{
+linea2 = fichero3.ReadLine();
+if (linea2 != null)
+Console.WriteLine( linea2 );
+}
+while (linea2 != null);
+fichero3.Close();
+
+
+// AÑÑADIR AL FINAL 
+StreamWriter fichero4;
+fichero4 = File.CreateText("prueba2.txt");
+fichero4.WriteLine("Primera línea");
+fichero4.Close();
+fichero4 = File.AppendText("prueba2.txt");
+fichero4.WriteLine("Segunda línea");
+fichero4.Close();
+
+
+// @ para no poner las barras de escape
+string nombreFichero = @"d:\ejemplos\ejemplo1.txt";
+// EXIsTS el fichero
+
+StreamReader fichero5;
+string nombre;
+while (true) {
+Console.Write( "Dime el nombre del fichero (\"fin\" para terminar): ");
+nombre = Console.ReadLine();
+if (nombre == "fin")
+break;
+if ( File.Exists(nombre) )
+{
+fichero5 = File.OpenText( nombre );
+Console.WriteLine("Su primera linea es: {0}",
+fichero5.ReadLine() );
+fichero5.Close();
+}
+else
+Console.WriteLine( "No existe!" );
+}
 }
 }
 }
---
+++ 30-ficheros-excepciones/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+namespace _30_ficheros_excepciones {
+    public class Ejemplo75 {
+
+        public static void Main () {
+            StreamReader fichero;
+            string nombre;
+            string linea;
+            Console.WriteLine ("Introduzca el nombre del fichero");
+            nombre = Console.ReadLine ();
+            try {
+                fichero = File.OpenText (nombre);
+                do {
+                    linea = fichero.ReadLine ();
+                    if (linea != null)
+                        Console.WriteLine (linea);
+                }
+                while (linea != null);
+                fichero.Close ();
+            } catch (Exception exp) {
+                Console.WriteLine ("Ha habido un error: {0}", exp.Message);
+                return;
+            }
+        }
+    }
+}
